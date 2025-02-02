e896e324fc29df06ceb383edd6475b057cbcfae0
Graham Strickland <grahamhstrickland@gmail.com>
2025-02-02 09:58:20 +0200

    Add more calculating examples/exercises

--- MathLean/Ch02/Calculating.lean
+++ MathLean/Ch02/Calculating.lean
@@ -5,7 +5,24 @@ example (a b c : ℝ) : a * b * c = b * (a * c) := by
   rw [mul_assoc b a c]
 
 example (a b c : ℝ) : c * b * a = b * (a * c) := by
-  sorry
+  rw [mul_comm c b]
+  rw [mul_assoc b c a]
+  rw [mul_comm c a]
 
 example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
-  sorry
+  rw [← mul_assoc a b c]
+  rw [mul_comm a b]
+  rw [mul_assoc b a c]
+
+example (a b c : ℝ) : a * b * c = b * c * a := by
+  rw [mul_assoc]
+  rw [mul_comm]
+
+example (a b c : ℝ) : a * (b * c) = b * (c * a) := by
+  rw [mul_comm]
+  rw [mul_assoc]
+
+example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
+  rw [mul_comm]
+  rw [mul_assoc]
+  rw [mul_comm c]
